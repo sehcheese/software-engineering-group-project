e43f6c3d728f327be9b2aeb882eaf901dd5faec3
Keith A Rice <darksideprogramming@gmail.com>
2015-04-06 21:44:50 -0400

    Think I finished the core functionality. Makes calls and seems to text fine, but need to test it on a real phone still

--- SafetyandSecurityApp/app/src/main/java/org/covenantcollege/safetyandsecurityapp/shuttle_Fragment.java
+++ SafetyandSecurityApp/app/src/main/java/org/covenantcollege/safetyandsecurityapp/shuttle_Fragment.java
@@ -9,13 +9,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
+import android.telephony.SmsManager;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 /**
  * Created by Keith on 23-Mar-15.
  */
 public class shuttle_Fragment extends Fragment {
     View rootview;
+
+    String shuttleNumber = "1234567890";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -27,14 +35,14 @@ public class shuttle_Fragment extends Fragment {
         callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                callShuttle(v);
+                callShuttle(rootview);
             }
         });
         Button textButton = (Button) rootview.findViewById(R.id.textButton);
         textButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textShuttle(v);
+                textShuttle(rootview);
             }
         });
         return rootview;
@@ -70,19 +78,26 @@ public class shuttle_Fragment extends Fragment {
     //calls the shuttle
     public void callShuttle(View view) {
         //Log.v("Test", "[*] testPhoneCall()");
-        Toast.makeText(getActivity(), "Calling?", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "Calling the Security Shuttle", Toast.LENGTH_SHORT).show();
         Intent callIntent = new Intent(Intent.ACTION_CALL);
-        callIntent.setData(Uri.parse("tel:1234567890"));
-        //startActivity(callIntent);
+        callIntent.setData(Uri.parse("tel:" + shuttleNumber));
+        startActivity(callIntent);
     }
 
     public void textShuttle(View view) {
-        String number = "1234567890";//edittText1.getText().toString();
-        String message = "test";//edittText2.getText().toString();
-/*        Intent i=new Intent(getApplicationContext(),MainActivity.class);
-        PendingIntent pIntent=PendingIntent.getActivity(getApplicationContext(), 0, i,0);
-        SmsManager sms=SmsManager.getDefault();
-        sms.sendTextMessage(number, null, message, pIntent, null);*/
-        Toast.makeText(getActivity(), "Message Sent !", Toast.LENGTH_LONG).show();
+        EditText nameField = (EditText)view.findViewById(R.id.shuttleName);
+        Spinner fromLocation = (Spinner)view.findViewById(R.id.locationSpinner);
+        Spinner toLocation = (Spinner)view.findViewById(R.id.destinationSpinner);
+        String message = "Hi im " + nameField.getText() + " and I'm feeling lazy. Come pick me up at " + fromLocation.getSelectedItem().toString() +
+                " and take me alllll the way to " + toLocation.getSelectedItem().toString() + ". Oh and make it snappy. Ain't nobody got time for waitin'! Hurry!";
+
+        System.out.println(message);
+
+
+        SmsManager sms = SmsManager.getDefault();
+        ArrayList<String> parts = sms.divideMessage(message); //we have to do this in case it is longer than a single message
+        sms.sendMultipartTextMessage(shuttleNumber, null, parts, null, null);
+
+        Toast.makeText(getActivity(), "Messaged Security Shuttle", Toast.LENGTH_LONG).show();
     }
 }
